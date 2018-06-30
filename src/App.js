d0b3e0a9f964972fe8a13bba4799641c5f2405ca
AndreyValoven <scorpion.and@ukr.net>
2018-06-30 09:44:14 +0300

    dynamic listing of  fake server playlists  data

--- src/App.js
+++ src/App.js
@@ -12,19 +12,35 @@ let fakeServerdata = {
     playLists: [
       {
         name: 'First',
-        songs: [{name: '1', duration: 1345}, {name: '2', duration: 2345}, {name: '3', duration: 5345}]
+        songs: [
+          {name: 'One ', duration: 1345}, 
+          {name: 'Some song', duration: 2345}, 
+          {name: 'New Life', duration: 5345}
+        ]
       },
       {
         name: 'Two',
-        songs: [{name: '1', duration: 1345}, {name: '2', duration: 2345}, {name: '3', duration: 5345}]
+        songs: [
+          {name: '1', duration: 1345}, 
+          {name: '2', duration: 2345}, 
+          {name: '3', duration: 5345}
+        ]
       },
       {
         name: 'Three',
-        songs: [{name: '1', duration: 1345}, {name: '2', duration: 2345}, {name: '3', duration: 5345}]
+        songs: [
+          {name: '1', duration: 1345}, 
+          {name: '2', duration: 2345}, 
+          {name: '3', duration: 5345}
+        ]
       },
       {
         name: 'Four',
-        songs: [{name: '1', duration: 1345}, {name: '2', duration: 2345}, {name: '3', duration: 5345}]
+        songs: [
+          {name: 'Litel song', duration: 1345}, 
+          {name: 'Brave', duration: 2345}, 
+          {name: 'Lite man', duration: 5345}
+        ]
       }
     ]
   }
@@ -34,7 +50,7 @@ class PlaylistCounter extends Component {
   render() {
     return (
       <div style={{ ...defaultStyle,width: '40%', display: 'inline-block'}}>
-        <h2>{this.props.palyLists.length} Text</h2>
+        <h2>{this.props.playLists.length} Text</h2>
       </div>
     );
   }
@@ -42,7 +58,7 @@ class PlaylistCounter extends Component {
 
 class HourCounter extends Component {
   render() {
-    let allSongs = this.props.palyLists.reduce((songs, eachPlayLists) => {
+    let allSongs = this.props.playLists.reduce((songs, eachPlayLists) => {
       return songs.concat(eachPlayLists.songs) 
     } , []);
     let totalDuration = allSongs.reduce((sum, eachSong) => {
@@ -69,14 +85,17 @@ class Filter extends Component {
 
 class PlayList extends Component {
   render() {
+    let playList = this.props.playList;
     return (
-      <div style={{ ...defaultStyle, width: '25%', display: 'inline-block'}}>
+      <div style={{ ...defaultStyle, width: '25%', display: 'inline-block', boxShadow: "2px 2px 2px #fff"}}>
         <img/>
-        <h3>PlayList Name</h3>
+        <h3>{playList.name}</h3>
         <ul>
-          <li>Song 1</li>
-          <li>Song 2</li>
-          <li>Song 3</li>
+          {
+            playList.songs.map(song => 
+              <li>{song.name}</li>
+            )
+          }
         </ul>
       </div>
     )
@@ -97,18 +116,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App">        
       {this.state.serverData.user ?
         <div>
           <h1>
             { this.state.serverData.user.name}'s PlayList
           </h1>
-          <PlaylistCounter palyLists={this.state.serverData.user.playLists}/>
-          <HourCounter palyLists={this.state.serverData.user.playLists}/>
+          <PlaylistCounter playLists={this.state.serverData.user.playLists}/>
+          <HourCounter playLists={this.state.serverData.user.playLists}/>
           <Filter/>
-          <PlayList/>
-          <PlayList/>
-          <PlayList/>
+          {
+            this.state.serverData.user.playLists.map(playList => 
+              <PlayList playList={playList}/>
+            )
+          }
         </div>: <h1>Loading...</h1>}
       </div>
     );
